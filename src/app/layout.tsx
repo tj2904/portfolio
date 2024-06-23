a5b6bbc7c25a6cb812f227172070a66de012b6b0
tj2904 <tj2904+github@gmail.com>
2024-06-23 21:17:14 +0100

    adds vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { type Metadata } from 'next'
-
+import { Analytics } from '@vercel/analytics/react'
 import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
@@ -30,6 +30,7 @@ export default function RootLayout({
       </head>
       <body className="flex min-h-full">
         <div className="w-full">{children}</div>
+        <Analytics />
       </body>
     </html>
   )
